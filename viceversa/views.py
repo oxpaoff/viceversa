66d69b1afd6874ca446f5871c766601aade00141
oxpaoff <1985oxpa@gmail.com>
2021-03-18 17:03:18 +0500

    'viceversta_final'

--- viceversa/views.py
+++ viceversa/views.py
@@ -7,5 +7,11 @@ def home(request):
 
 def reverse(request):
     get_text = request.GET['usertext']
+    amount = 0
+    for i in get_text.split(' '):
+        amount += 1
+    text = 'Original text has {0} words'.format(amount)
     reversed_text = get_text[::-1]
-    return render(request, 'reverse.html', {'usertext':reversed_text})
+
+    return render(request, 'reverse.html', {'reverse': reversed_text, 'use': get_text, 'number':
+        text})
